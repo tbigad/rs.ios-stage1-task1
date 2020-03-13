3fdcec6c5ee57162d0285967c0817e007f3e5768
Pavel Nadolski <pavel.nadolski@itt-us.com>
2020-03-13 16:30:35 +0300

    complite task1

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.h
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.h
@@ -4,5 +4,7 @@
 @interface MiniMaxArrayConverter : NSObject
 
 - (NSArray <NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array;
-
+- (NSArray<NSNumber*>*)sortFromMinToMax:(NSArray<NSNumber*>*)array withLenght:(int)count;
+- (NSArray<NSNumber*>*)sortFromMaxToMin:(NSArray<NSNumber*>*)array withLenght:(int)count;
+- (NSNumber*)sumOfElements:(NSArray<NSNumber*>*)array;
 @end
--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,33 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    
+    NSArray<NSNumber *> *min = [self sortFromMinToMax:array withLenght:array.count - 1];
+    NSArray<NSNumber *> *max = [self sortFromMaxToMin:array withLenght:array.count - 1];
+    NSNumber* minValue = [self sumOfElements:min];
+    NSNumber* maxValue = [self sumOfElements:max];
+    
+    NSArray<NSNumber*>* ret = [[NSArray alloc]initWithObjects:minValue,maxValue, nil];
+    return ret;
 }
 
+- (NSArray<NSNumber *> *)sortFromMaxToMin:(NSArray<NSNumber *> *)array withLenght:(int)count {
+    NSSortDescriptor *aSortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:NO];
+    
+    return [[array sortedArrayUsingDescriptors:[NSArray arrayWithObject:aSortDescriptor]] subarrayWithRange:NSMakeRange(0, count)];
+}
+
+- (NSArray<NSNumber *> *)sortFromMinToMax:(NSArray<NSNumber *> *)array withLenght:(int)count {
+    NSSortDescriptor *aSortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES];
+    
+    return [[array sortedArrayUsingDescriptors:[NSArray arrayWithObject:aSortDescriptor]] subarrayWithRange:NSMakeRange(0, count)];
+}
+
+- (NSNumber *)sumOfElements:(NSArray<NSNumber *> *)array {
+    NSInteger sum = 0;
+    for (NSNumber *num in array) {
+      sum += [num intValue];
+    }
+    return [[NSNumber alloc]initWithLong:sum];
+}
 @end
