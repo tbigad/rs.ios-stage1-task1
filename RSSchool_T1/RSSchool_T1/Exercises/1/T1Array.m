01e4541ceb06d6ae953dbe8d04a245eca7c5d5e7
Pavel Nadolski <pavel.nadolski@itt-us.com>
2020-03-13 15:00:39 +0300

    complite task0

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,33 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    if (sadArray.count == 0) {
+        return @[];
+    }
+    
+    long minIndex = 0;
+    bool isComplite = false;
+    NSMutableArray *ret = [[NSMutableArray alloc] init];
+    [ret addObjectsFromArray:sadArray];
+    
+    while (!isComplite) {
+        isComplite = true;
+        for (int i = 1; i < ret.count - 1; i++) {
+            if ( [ret[i] isEqual:ret.firstObject] || [ret[i] isEqual:ret.lastObject] ) {
+                continue;
+            }
+            int sum = [ret[i - 1] intValue] + [ret[i + 1] intValue];
+            if (sum < [ret[i] intValue]) {
+                if([ret containsObject:ret[i]]){
+                    [ret removeObjectAtIndex:i];
+                    isComplite = false;
+                    break;
+                }
+            }
+        }
+    }
+    
+    return ret;
 }
 
 @end
