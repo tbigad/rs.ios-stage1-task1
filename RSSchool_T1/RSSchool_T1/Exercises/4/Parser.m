11b4a07918a51bf13f7dddba6cd5deb579d35e51
Pavel Nadolski <tbigad@gmail.com>
2020-03-15 15:56:07 +0300

    task4

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,33 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    NSString* openBracket = @"(<[";
+    NSString* closeBracket = @")>]";
+    NSMutableString * mutableString = [NSMutableString stringWithString:string];
+    NSMutableArray<NSString*>* ret = [NSMutableArray arrayWithCapacity:5];
+    int repeat = 0;
+    for (int i = 0; i < [string length]; i++){
+        for(int j = 0; j < 3; j++){
+            if ([mutableString characterAtIndex:i] == [openBracket characterAtIndex:j]) {
+                for (int s = i+1; s<[string length]; s++) {
+                    if ([mutableString characterAtIndex:s] == [openBracket characterAtIndex:j]) {
+                        repeat++;
+                        continue;
+                    }
+                    if ([mutableString characterAtIndex:s] == [closeBracket characterAtIndex:j] && repeat!=0) {
+                        repeat--;
+                        continue;
+                    }
+                    if ([mutableString characterAtIndex:s] == [closeBracket characterAtIndex:j] && repeat==0) {
+                        [ret addObject:[string substringWithRange:NSMakeRange(i+1, s-i-1)]];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return ret;
 }
 
 @end
+
