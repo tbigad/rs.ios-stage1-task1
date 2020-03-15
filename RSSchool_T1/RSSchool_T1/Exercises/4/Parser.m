f3d6884acefacca1c77f38e875a1a5d4fe5732c2
Pavel Nadolski <tbigad@gmail.com>
2020-03-15 15:57:56 +0300

    task4

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,24 +4,24 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    NSString* openBracket = @"(<[";
-    NSString* closeBracket = @")>]";
+    NSString* openBracketSymbols = @"(<[";
+    NSString* closeBracketSymbols = @")>]";
     NSMutableString * mutableString = [NSMutableString stringWithString:string];
     NSMutableArray<NSString*>* ret = [NSMutableArray arrayWithCapacity:5];
-    int repeat = 0;
+    int counter = 0;
     for (int i = 0; i < [string length]; i++){
         for(int j = 0; j < 3; j++){
-            if ([mutableString characterAtIndex:i] == [openBracket characterAtIndex:j]) {
+            if ([mutableString characterAtIndex:i] == [openBracketSymbols characterAtIndex:j]) {
                 for (int s = i+1; s<[string length]; s++) {
-                    if ([mutableString characterAtIndex:s] == [openBracket characterAtIndex:j]) {
-                        repeat++;
+                    if ([mutableString characterAtIndex:s] == [openBracketSymbols characterAtIndex:j]) {
+                        counter++;
                         continue;
                     }
-                    if ([mutableString characterAtIndex:s] == [closeBracket characterAtIndex:j] && repeat!=0) {
-                        repeat--;
+                    if ([mutableString characterAtIndex:s] == [closeBracketSymbols characterAtIndex:j] && counter != 0) {
+                        counter--;
                         continue;
                     }
-                    if ([mutableString characterAtIndex:s] == [closeBracket characterAtIndex:j] && repeat==0) {
+                    if ([mutableString characterAtIndex:s] == [closeBracketSymbols characterAtIndex:j] && counter == 0) {
                         [ret addObject:[string substringWithRange:NSMakeRange(i+1, s-i-1)]];
                         break;
                     }
